5b02a36ba1c5c91206db9e185daf8c2b33950752
Samsuik <kfian294ma4@gmail.com>
2023-12-14 21:21:55 +0000

    Sakura Fork

---
+++ README.md
@@ -0,0 +1,3 @@
+Sakura Fork
+===========
+This is an example of a [Sakura](http://github.com/Samsuik/Sakura) fork based on the PaperMC [ForkTest](https://github.com/papermc/paperweight-examples).
---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
+
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.11"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":sakurafork-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    useStandardUpstream("sakura") {
+        url.set(github("Samsuik", "Sakura"))
+        ref.set(providers.gradleProperty("sakuraRef"))
+
+        withStandardPatcher {
+            // source location this is required for case-sensitive file systems
+            // baseName makes the assumption that the source type is capitalised
+            apiSourceDirPath.set("$name-api")
+            serverSourceDirPath.set("$name-server")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("sakurafork-api"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("sakurafork-server"))
+        }
+    }
+}
